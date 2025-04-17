6e51867803a953156169bdfde039f567d27e6915
CodyCundiffTV2025 <cody.w.cundiff@gmail.com>
2025-04-17 11:28:15 -0700

    Ditto

--- TVPoke/Pokemon/Ditto.py
+++ TVPoke/Pokemon/Ditto.py
@@ -5,8 +5,8 @@ class Ditto(Normal):
     def __init__(self):
         moves = [
             Move("Transform", "NORMAL", 0),
-            Move("Hyper Beam", "NORMAL", 150),  
+            Move("Hyper Beam", "NORMAL", 300),  
             Move("Body Slam", "NORMAL", 85),   
             Move("Double-Edge", "NORMAL", 120),     
         ]
-        super().__init__("ditto", 48, moves, "./TVPoke/Pokemon/imgs/ditto.webp")
+        super().__init__("ditto", 10, moves, "./TVPoke/Pokemon/imgs/ditto.png")
